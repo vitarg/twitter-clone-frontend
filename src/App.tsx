b51ae8c8c293d73cccb9fae0737d978bee937e77
Mansur <vitrigovm@gmail.com>
2022-02-06 13:29:37 +0300

    1. добавлен файл для кастомной темы mui 2. добавлен файл для страницы входа

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@mui/material";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Button>Button</Button>
+    </div>
+  );
 }
 
 export default App;
